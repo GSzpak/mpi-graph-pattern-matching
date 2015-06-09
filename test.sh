4307251612e40a4d83b4d604b2229b459528341b
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2015-06-09 12:40:11 +0200

    Test script added

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+trap "exit" INT
+for file in data/*.in
+do
+    base=${file%.*}
+    echo $base
+    for i in {1..100}
+    do
+        echo $i; mpirun -np 8 par/gpm-par.exe $file out
+        sort out > out2
+        diff out2 ${base}.out
+    done
+done
+rm out out2
